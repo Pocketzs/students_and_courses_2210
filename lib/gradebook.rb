9d107c7788b8148b0b80c83a5f5f717db3311011
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-27 16:18:00 -0500

    test: Add and pass gradebook list_grades test and add find_students_in_grade_range test

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -18,4 +18,17 @@ class Gradebook
       student.grade < threshold
     end
   end
+  # The University of Denver would like you to implement additional functionality. 
+  # They would like to be able to track all the grades across all courses. 
+  # They would also like to be able to find all students across all courses 
+  # that have a grade in a given range.
+  def list_grades
+    grades_by_course = {}
+    courses.each do |course|
+      grades_by_course[course.name] = course.students.map do |student|
+        student.grade
+      end
+    end
+    grades_by_course
+  end
 end
