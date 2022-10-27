e77f01a495e21b7a65038058c58f94faa7bbdd29
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-27 10:08:11 -0500

    test: Add and pass student exists test

---
+++ lib/student.rb
@@ -0,0 +1,6 @@
+class Student
+  def initialize(attributes)
+    @name = attributes[:name]
+    @age = attributes[:age]
+  end
+end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,4 +1,12 @@
 require './lib/student'
+require 'rspec'
 
 RSpec.describe Student do
+  describe '#initialize' do
+    it 'exists' do
+      student = Student.new({name: "Morgan", age: 21}) 
+
+      expect(student).to be_a Student
+    end
+  end
 end
