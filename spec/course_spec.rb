438e11fd07cbbcea2b0371bb771d4a606f83d621
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-27 10:43:39 -0500

    test: Add and pass course names test

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -9,5 +9,13 @@ describe Course do
 
       expect(course).to be_a(Course)
     end
+
+    it 'can have names' do
+      course_1 = Course.new("Calculus", 2)
+      course_2 = Course.new("History", 3)
+
+      expect(course_1.name).to eq("Calculus")
+      expect(course_2.name).to eq("History")
+    end
   end
 end
