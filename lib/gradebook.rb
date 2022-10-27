4a9d0525229488cff9927dd5b4a715c4b2b4b95c
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-27 11:29:34 -0500

    test: Add and pass gradebook list_students test

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,8 +1,15 @@
 class Gradebook
   attr_reader :instructor,
               :courses
+
   def initialize(attributes)
     @instructor = attributes[:instructor]
     @courses = attributes[:courses]
+  end
+
+  def list_students
+    courses.map do |course|
+      course.students
+    end.flatten
   end
 end
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -35,4 +35,26 @@ describe Gradebook do
       expect(gradebook2.courses).to eq [history, chemistry]
     end
   end
+
+  describe '#list students' do
+    it 'can list all the students in its courses' do
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jack", age: 25})
+      student3 = Student.new({name: "Joey", age: 22})
+      student4 = Student.new({name: "Christine", age: 26})
+      calculus = Course.new("Calculus", 2)
+      geometry = Course.new("Geometry", 3)
+      history = Course.new("History", 10)
+      chemistry = Course.new("Chemistry", 1)
+
+      calculus.enroll(student1)
+      calculus.enroll(student2)
+      geometry.enroll(student3)
+      history.enroll(student4)
+
+      gradebook = Gradebook.new({instructor: "McDaniels", courses: [calculus, geometry, history, chemistry]})
+      
+      expect(gradebook.list_students).to eq [student1, student2, student3, student4]
+    end
+  end
 end
